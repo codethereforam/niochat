4edc7d1b48a75d98b6e1dedc9cb922d02e591b7a
codethereforam <codethereforam@gmail.com>
2018-11-12 10:24:50 +0800

    modify: refactor DecryptTool

--- src/main/java/priv/thinkam/niochat/util/DecryptTool.java
+++ src/main/java/priv/thinkam/niochat/util/DecryptTool.java
@@ -1,9 +1,10 @@
 package priv.thinkam.niochat.util;
 
 import java.io.IOException;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 /**
  * @author thinkam
@@ -15,11 +16,11 @@ public class DecryptTool {
 			System.out.println("please enter the path of file in args[0]");
 			return;
 		}
-		Path path = FileSystems.getDefault().getPath(args[0].trim());
-		try {
-			Files.lines(path).forEach(line -> {
+		Path path = Paths.get(args[0].trim());
+		try (Stream<String> lines = Files.lines(path)) {
+			lines.forEach(line -> {
 				String decryptedLine = line;
-				if(StringUtils.isNotBlank(line)) {
+				if (StringUtils.isNotBlank(line)) {
 					try {
 						decryptedLine = AESUtils.decrypt(line, "123456789123456789");
 					} catch (Exception e) {
